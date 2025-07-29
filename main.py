761773187297ccefb2c13dd69906ca75a4e5f6f9
Shak Ragoler <shak.ragoler@gmail.com>
2025-07-29 10:24:15 -0700

    Adding an undo function

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ class Game:
         self.height = 0
         self.width = 0
         self.player = 'A'
+
         self.vertical = []
         self.horizontal = []
         self.inner = []
         self.A = 0
         self.B = 0
+
         self.remaining = 0
         self.history = []
 
@@ -15,11 +17,13 @@ class Game:
         self.height = height
         self.width = width
         self.player = 'A'
+
         self.vertical = [['*' for j in range(width)] for i in range(height - 1)]
         self.horizontal = [['*' for j in range(width - 1)] for i in range(height)]
         self.inner = [['*' for j in range(width - 1)] for i in range(height - 1)]
         self.A = 0
         self.B = 0
+
         self.remaining = width * (height - 1) + height * (width - 1)
         self.history = []
 
@@ -34,37 +38,92 @@ class Game:
                 and self.vertical[y][x] != '*' and self.vertical[y][x + 1] != '*'
                 and self.horizontal[y][x] != '*' and self.horizontal[y + 1][x] != '*'):
             self.inner[y][x] = self.player
+
             if self.player == 'A':
                 self.A += 1
             else:
                 self.B += 1
             return True
+
         return False
 
     def place_vertical(self, x, y):
         if 0 <= y < self.height - 1 and 0 <= x < self.width:
+            self.history.append((x, y, 'V'))
             self.vertical[y][x] = '|'
             self.remaining -= 1
+
             return True
         return False
 
     def place_horizontal(self, x, y):
         if 0 <= y < self.height and 0 <= x < self.width - 1:
+            self.history.append((x, y, 'H'))
             self.horizontal[y][x] = '—'
             self.remaining -= 1
+
             return True
         return False
 
     def get_winner(self):
         if self.remaining > 0:
             return 'Undecided'
+
         elif self.A > self.B:
             return 'A'
+
         elif self.B > self.A:
             return 'B'
+
         else:
             return 'Tie'
 
+    def undo(self):
+        if len(self.history) > 0:
+            x, y, direction = self.history.pop()
+
+            if direction == 'V':
+                self.vertical[y][x] = '*'
+                self.remaining += 1
+
+                if self.inner[y][x] == 'A':
+                    self.A -= 1
+                    self.inner[y][x] = '*'
+
+                elif self.inner[y][x] == 'B':
+                    self.B -= 1
+                    self.inner[y][x] = '*'
+
+                if self.inner[y][x + 1] == 'A':
+                    self.A -= 1
+                    self.inner[y][x + 1] = '*'
+
+                elif self.inner[y][x + 1] == 'B':
+                    self.B -= 1
+                    self.inner[y][x + 1] = '*'
+
+            elif direction == 'H':
+                self.horizontal[y][x] = '*'
+                self.remaining += 1
+
+                if self.inner[y][x] == 'A':
+                    self.A -= 1
+                    self.inner[y][x] = '*'
+
+                elif self.inner[y][x] == 'B':
+                    self.B -= 1
+                    self.inner[y][x] = '*'
+
+                if self.inner[y + 1][x] == 'A':
+                    self.A -= 1
+                    self.inner[y + 1][x] = '*'
+
+                elif self.inner[y + 1][x] == 'B':
+                    self.B -= 1
+                    self.inner[y + 1][x] = '*'
+
+            return True
+        return False
 
 def main():
     print('Welcome to the Dots and Boxes solver!')
